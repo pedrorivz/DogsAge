7f19b617ba54c8f9d81874e427af76c07b99d11e
phsantosit <phsantosit@gmail.com>
2022-02-04 18:15:35 -0300

    Now array with characters limits has been added

--- DogAgeV1/ViewController.swift
+++ DogAgeV1/ViewController.swift
@@ -14,16 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultText: UITextView!
     
     @IBAction func discoverAge(_ sender: Any) {
-        let age = Int(dogAge.text!)! * 7
-        let plainAge = String(age)
-        self.resultText.text = "Your human dog's age is: " + plainAge
+        guard let dogAgeString = dogAge.text,
+              let ageInt = Int(dogAgeString)
+        else {
+            return
+        }
+        
+        let age = String(ageInt * 7)
+        self.resultText.text = "Your human dog's age is: \(age)"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        dogAge.delegate = self
     }
 
 
+}
+
+extension ViewController:UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map{String($0)}
+        
+        if !numbers.contains(string) && string != "" {
+            return false
+        }
+        return true
+    }
 }
 
